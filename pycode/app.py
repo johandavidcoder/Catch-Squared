a4e45f8a2fd8a82a534ab3f4a2b7e07f1bf05b00
johandavidcoder <johandavid.one@gmail.com>
2026-02-02 22:22:30 +0530

    Refactor Flask app configuration and run settings
    Updated path configuration for template and static folders to use BASE_DIR. Enabled debug mode and changed the host and port for the Flask app.

--- pycode/app.py
+++ pycode/app.py
@@ -1,14 +1,15 @@
 import os
 from flask import Flask, render_template
 
-# Reconfigure paths to look up one level from 'pycode'
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
 app = Flask(__name__, 
-            template_folder=os.path.join('..', 'templates'),
-            static_folder=os.path.join('..', 'static'))
+            template_folder=os.path.join(BASE_DIR, 'templates'),
+            static_folder=os.path.join(BASE_DIR, 'static'))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5001, host='0.0.0.0')
